2cfbb3e57674535a965bf1fc0cb85f47faa443fd
Maheswaran2512 <devamahesh8116@gmail.com>
2025-03-12 17:17:04 +0530

    pdf

--- js/cv.js
+++ js/cv.js
@@ -1,6 +1,6 @@
 function downloadCV() {
     const link = document.createElement('a');
-    link.href = 'Resume.pdf'; // Replace with the actual path to your CV
+    link.href = 'src/Resume.pdf'; // Replace with the actual path to your CV
     link.download = 'Maheswaran D CV.pdf'; // Set the filename for download (optional)
     link.style.display = 'none'; // Hide the link element
     document.body.appendChild(link);
